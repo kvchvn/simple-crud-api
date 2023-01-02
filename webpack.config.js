76ef981c9818c08de9793a8fc16316c628f63bc0
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-02 22:46:50 +0300

    build: configure webpack and typescript

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config = {
+  context: resolve(__dirname, 'src'),
+  entry: './index.ts',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+};
+
+export default config;
