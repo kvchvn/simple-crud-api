397e21479c0023b6a8e3078e9a323edd75440d15
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-04 18:00:10 +0300

    feat: implement getUserById function

--- src/data.ts
+++ src/data.ts
@@ -8,3 +8,21 @@ const ALL_USERS: User[] = [
 ];
 
 export const getAllUsers = () => ALL_USERS;
+
+export const getUserById = (userId: string): OperationResult => {
+  //TODO: check if id is valid
+  const user = ALL_USERS.find((user) => user.id === userId);
+  if (user) {
+    return {
+      isDone: true,
+      statusCode: 200,
+      data: user,
+    };
+  } else {
+    return {
+      isDone: false,
+      statusCode: 404,
+      message: `User with such id doesn't exist.`,
+    };
+  }
+};
