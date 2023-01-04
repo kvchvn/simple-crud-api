c6afd39629134135a1d06973c50b851de1fb785f
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-04 17:59:35 +0300

    feat: create types for operations with array of users

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,17 @@ export interface User {
   age: number;
   hobbies: string[];
 }
+
+export interface GoodResult {
+  isDone: true;
+  statusCode: number;
+  data: User;
+}
+
+export interface BadResult {
+  isDone: false;
+  statusCode: number;
+  message: string;
+}
+
+export type OperationResult = GoodResult | BadResult;
