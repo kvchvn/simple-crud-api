60e4be224d9c2ff54f1b68b9e44686e0466bfe37
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-04 18:18:52 +0300

    feat: handle case of invalid url

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import { createServer } from 'http';
+import {
+  sendDataInJSON,
+  sendError,
+  sendInternalServerError,
+  sendInvalidBodyError,
+  sendInvalidUrlError,
+} from './helpers.js';
+import { Endpoints } from './types.js';
 const server = createServer();
 
 server.on('request', (req, res) => {
+  try {
+    const { url, method } = req;
+    const urlArray = url ? url.split('/').slice(1) : [''];
+    const [reqPath, reqSubPath, id] = urlArray;
+
+    // Invalid url
+    if (
+      reqPath !== Endpoints.Api ||
+      reqSubPath !== Endpoints.Users ||
+      id === '' ||
+      urlArray.length > 3
+    ) {
+      sendInvalidUrlError(res);
+    }
+  } catch {
+    sendInternalServerError(res);
+  }
 });
 
 server.listen(4000);
