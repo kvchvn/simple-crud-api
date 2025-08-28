1a7b9cbe5852b716144de032669afcf08f6a7025
Anton Kachan <aa.kvchvn@gmail.com>
2025-08-28 12:16:43 +0300

    init: set up eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from "eslint-plugin-unused-imports";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      }
+    },
+    plugins: {
+      ts: tseslint.plugin,
+      'unused-imports': unusedImports,
+      'simple-import-sort': simpleImportSort,
+      prettier,
+    },
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+    ],
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "simple-import-sort/imports": "error",
+      "prettier/prettier": "error",
+    }
+  },
+  {
+    files: ["src/**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"]
+  },
+]);
